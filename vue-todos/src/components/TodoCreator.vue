c770c3520b2da11dde789aa590bc0c52ea2212de
yommoodev <jvillanueva2698@gmail.com>
2025-03-19 11:45:01 +0800

    Initial commit, with Components already hehe

---
+++ vue-todos/src/components/TodoCreator.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref, reactive } from "vue";
+
+const todo = ref('Testing');
+
+const todoState = reactive({
+    todo: "Testing reactive",
+});
+</script>
+
+<template>
+    <div class="input-wrap">
+        <input type="text" v-model="todoState.todo"/>
+        <button>Create</button>
+    </div>
+    <p>{{ todoState.todo }}</p>
+</template>
+
+
+
+<style lang="scss" scoped>
+.input-wrap {
+  display: flex;
+  transition: 250ms ease;
+  border: 2px solid #41b080;
+
+  &:focus-within {
+    box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
+      0 -2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+
+  input {
+    width: 100%;
+    padding: 8px 6px;
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  button {
+    padding: 8px 16px;
+    border: none;
+  }
+}
+</style>
