e8c8f4db90cfbc9bfdd384eccde4472ec7be430b
yommoodev <jvillanueva2698@gmail.com>
2025-03-19 20:41:33 +0800

    added delete function

--- vue-todos/src/views/TodosView.vue
+++ vue-todos/src/views/TodosView.vue
@@ -25,6 +25,10 @@ const toggleEditTodo = (todoPos) => {
 const updateTodo = (todoVal, todoPos) => {
   todoList.value[todoPos].todo = todoVal;
 }
+
+const deleteTodo = (todoId) => {
+  todoList.value = todoList.value.filter((todo) => todo.id !== todoId);
+}
 </script>
 
 <template>
@@ -34,7 +38,7 @@ const updateTodo = (todoVal, todoPos) => {
       <template #button-content>Create</template>
     </TodoCreator>
     <ul class="todo-list" v-if="todoList.length > 0">
-      <TodoItem v-for="(todo, index) in todoList" :todo="todo" :index="index" @toggle-complete="toggleTodoComplete" @edit-todo="toggleEditTodo" @update-todo="updateTodo"/>
+      <TodoItem v-for="(todo, index) in todoList" :todo="todo" :index="index" @toggle-complete="toggleTodoComplete" @edit-todo="toggleEditTodo" @update-todo="updateTodo" @delete-todo="deleteTodo"/>
     </ul>
     <p v-else class="todos-msg">
       <Icon icon="noto-v1:sad-but-relieved-face" />
