dcf0c6662cc163fa1d80df091060a883f1cd2b71
yommoodev <jvillanueva2698@gmail.com>
2025-03-19 21:05:17 +0800

    trying the router

--- vue-todos/src/router/index.js
+++ vue-todos/src/router/index.js
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/testing',
+      name: 'testing',
+      component: () => import('../views/TestingView.vue'),
+    }
   ],
 })
 
